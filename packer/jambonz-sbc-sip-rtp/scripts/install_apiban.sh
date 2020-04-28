4c682d2c97cad0d7800898d4e74a8b79b2639003
Dave Horton <daveh@beachdognet.com>
2020-04-28 19:42:05 -0400

    install apiban

--- packer/jambonz-sbc-sip-rtp/scripts/install_apiban.sh
+++ packer/jambonz-sbc-sip-rtp/scripts/install_apiban.sh
@@ -8,5 +8,6 @@ sudo cp /tmp/config.json /usr/local/bin/apiban/config.json
 sudo chmod 0644 /usr/local/bin/apiban/config.json
 sudo cp /tmp/apiban.logrotate /etc/logrotate.d/apiban-client
 sudo chmod 0644 /etc/logrotate.d/apiban-client
-sudo /usr/local/bin/apiban/apiban-iptables-client
-echo "*/4 * * * * root /usr/local/bin/apiban/apiban-iptables-client >/dev/null 2>&1" | sudo tee -a /etc/crontab
+cd /usr/local/bin/apiban/
+sudo ./apiban-iptables-client
+echo "*/4 * * * * root cd /usr/local/bin/apiban && ./apiban-iptables-client >/dev/null 2>&1" | sudo tee -a /etc/crontab
