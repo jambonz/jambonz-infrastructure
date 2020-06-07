f9196602ea4ff6f9b757625e5bf41f80d80e2b86
Dave Horton <daveh@beachdognet.com>
2020-06-07 14:54:11 -0400

    use a systemd service to manage pm2, and configure rtpengine with knowledge of private and public interfaces

--- packer/jambonz-sbc-sip-rtp/scripts/install_app.sh
+++ packer/jambonz-sbc-sip-rtp/scripts/install_app.sh
@@ -25,6 +25,3 @@ sudo -u admin bash -c "pm2 install pm2-logrotate"
 sudo -u admin bash -c "pm2 set pm2-logrotate:max_size 1G"
 sudo -u admin bash -c "pm2 set pm2-logrotate:retain 5"
 sudo -u admin bash -c "pm2 set pm2-logrotate:compress true"
-
-# add entries to /etc/crontab to start the app on reboot
-echo "@reboot admin /usr/bin/pm2 start /home/admin/apps/ecosystem.config.js" | sudo tee -a /etc/crontab
