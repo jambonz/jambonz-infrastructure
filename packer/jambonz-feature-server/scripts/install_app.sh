5ef3b96d5da18d1cadc8770bbd46d389c7632e4b
Dave Horton <daveh@beachdognet.com>
2020-06-18 10:03:36 -0400

    add script to clear calls on FS > 3 hours in duration

--- packer/jambonz-feature-server/scripts/install_app.sh
+++ packer/jambonz-feature-server/scripts/install_app.sh
@@ -7,8 +7,12 @@ cp /tmp/ecosystem.config.js apps
 cd apps
 
 git clone https://github.com/jambonz/jambonz-feature-server.git -b ${VERSION}
+git clone https://github.com/jambonz/fsw-clear-old-calls.git
 
 cd /home/admin/apps/jambonz-feature-server && npm install
+cd /home/admin/apps/fsw-clear-old-calls && npm install && sudo npm install -g .
+
+echo "0 *	* * *	root    fsw-clear-old-calls --password JambonzR0ck$ >> /var/log/fsw-clear-old-calls.log 2>&1" | sudo tee -a /etc/crontab
 
 sudo -u admin bash -c "pm2 install pm2-logrotate"
 sudo -u admin bash -c "pm2 set pm2-logrotate:max_size 1G"
--- packer/jambonz-feature-server/scripts/install_drachtio.sh
+++ packer/jambonz-feature-server/scripts/install_drachtio.sh
@@ -11,9 +11,6 @@ git submodule update --init --recursive
 ./autogen.sh && mkdir -p build && cd $_ && ../configure CPPFLAGS='-DNDEBUG' && make -j 4 && sudo make install
 sudo mv /tmp/drachtio.conf.xml /etc
 sudo mv /tmp/drachtio.service /etc/systemd/system
-sudo mv /tmp/vimrc.local /etc/vim/vimrc.local
 sudo chmod 644 /etc/drachtio.conf.xml
 sudo chmod 644 /etc/systemd/system/drachtio.service
-sudo chmod 644 /etc/vim/vimrc.local
-sudo chown root:root /etc/drachtio.conf.xml /etc/systemd/system/drachtio.service /etc/vim/vimrc.local
 sudo systemctl enable drachtio
