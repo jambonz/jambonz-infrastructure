58af90b07df4b026163a7e06ec55d87752611702
Dave Horton <daveh@beachdognet.com>
2020-09-28 14:50:07 -0400

    feature server packer: remove old .mp3 files periodically, build AMI with larger instance type for speed

--- packer/jambonz-feature-server/scripts/install_app.sh
+++ packer/jambonz-feature-server/scripts/install_app.sh
@@ -9,12 +9,15 @@ cd apps
 git clone https://github.com/jambonz/jambonz-feature-server.git -b ${VERSION}
 git clone https://github.com/jambonz/fsw-clear-old-calls.git
 
-cd /home/admin/apps/jambonz-feature-server && npm install
+cd /home/admin/apps/jambonz-feature-server && sudo npm install --unsafe-perm
 cd /home/admin/apps/fsw-clear-old-calls && npm install && sudo npm install -g .
 
-echo "0 *	* * *	root    fsw-clear-old-calls --password JambonzR0ck$ >> /var/log/fsw-clear-old-calls.log 2>&1" | sudo tee -a /etc/crontab
+echo "0 *	* * * root    fsw-clear-old-calls --password JambonzR0ck$ >> /var/log/fsw-clear-old-calls.log 2>&1" | sudo tee -a /etc/crontab
+echo "0 1	* * * root    find /tmp -name \"*.mp3\" -mtime +2 -exec rm {} \; > /dev/null 2>&1" | sudo tee -a /etc/crontab
 
 sudo -u admin bash -c "pm2 install pm2-logrotate"
 sudo -u admin bash -c "pm2 set pm2-logrotate:max_size 1G"
 sudo -u admin bash -c "pm2 set pm2-logrotate:retain 5"
 sudo -u admin bash -c "pm2 set pm2-logrotate:compress true"
+
+sudo chown -R admin:admin  /home/admin/apps
