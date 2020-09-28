8160b07c69328a1c02443814cf78598fe5bea829
Dave Horton <daveh@beachdognet.com>
2020-09-28 14:51:23 -0400

    update node to v12, change app folder ownership to admin

--- packer/jambonz-sbc-sip-rtp/scripts/install_app.sh
+++ packer/jambonz-sbc-sip-rtp/scripts/install_app.sh
@@ -13,14 +13,16 @@ git clone https://github.com/jambonz/sbc-call-router.git -b ${VERSION}
 git clone https://github.com/jambonz/jambonz-api-server.git -b ${VERSION}
 git clone https://github.com/jambonz/jambonz-webapp.git -b ${VERSION}
 
-cd /home/admin/apps/sbc-inbound && npm install
-cd /home/admin/apps/sbc-outbound && npm install
-cd /home/admin/apps/sbc-registrar && npm install
-cd /home/admin/apps/sbc-call-router && npm install
-cd /home/admin/apps/jambonz-api-server && npm install
-cd /home/admin/apps/jambonz-webapp && npm install && npm run build
+cd /home/admin/apps/sbc-inbound && sudo npm install --unsafe-perm
+cd /home/admin/apps/sbc-outbound && sudo npm install --unsafe-perm
+cd /home/admin/apps/sbc-registrar && sudo npm install --unsafe-perm
+cd /home/admin/apps/sbc-call-router && sudo npm install --unsafe-perm
+cd /home/admin/apps/jambonz-api-server && sudo npm install --unsafe-perm
+cd /home/admin/apps/jambonz-webapp && sudo npm install --unsafe-perm && npm run build
 
 sudo -u admin bash -c "pm2 install pm2-logrotate"
 sudo -u admin bash -c "pm2 set pm2-logrotate:max_size 1G"
 sudo -u admin bash -c "pm2 set pm2-logrotate:retain 5"
 sudo -u admin bash -c "pm2 set pm2-logrotate:compress true"
+
+sudo chown -R admin:admin  /home/admin/apps
