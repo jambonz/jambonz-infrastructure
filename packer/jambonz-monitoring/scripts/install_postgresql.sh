4e1993b66beaadede2ad4532b5c7147080ed95c8
Dave Horton <daveh@beachdognet.com>
2020-11-16 12:34:09 -0500

    changes to build on buster

--- packer/jambonz-monitoring/scripts/install_postgresql.sh
+++ packer/jambonz-monitoring/scripts/install_postgresql.sh
@@ -6,7 +6,7 @@ DB_USER=$2
 DB_PASS=$3
 
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | sudo apt-key add -
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" > /etc/apt/sources.list.d/postgresql.list'
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/postgresql.list'
 sudo apt-get update
 sudo apt-get install -y postgresql-12
 sudo systemctl daemon-reload
