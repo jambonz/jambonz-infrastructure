bcfbcdaf2f7f33e62ff7dad0846db4b03dcf0ef6
Antony Jukes <ajukes@vibecoms.co.uk>
2021-12-03 15:40:18 +0000

    Feature - Packer: Add User Variables for MRF Profile, Preffered Codecs & Datadog Key (#10)
    * -gitignore for intellij
    -user variable for preferred_codec_list
    -user variable for datadog_api_key
    -user variable for mediaserver_name
    
    * added new user variables to template.json
    
    * added datadog_key for sbc-sip
    
    * added datadog_key for sbc-rtp

--- packer/jambonz-feature-server/scripts/install_datadog.sh
+++ packer/jambonz-feature-server/scripts/install_datadog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "$1" == "yes" ]; then 
+if [ "$DD_INSTALL" == "yes" ] && [ "$DD_KEY" != "" ]; then
   echo installing datadog...
 
-  DD_INSTALL_ONLY=true DD_API_KEY=your-dd-key-here bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
+  DD_INSTALL_ONLY=true DD_API_KEY=${DD_KEY} bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
 fi
