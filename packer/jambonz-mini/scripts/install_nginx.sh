c66e2fa8f2bbf17fb6f87e75fc5618e8a9996b0f
Antony Jukes <ajukes@vibecoms.co.uk>
2023-01-13 21:18:39 +0000

    Feature/add basic auth jaeger (#38)
    * Implemented Jaeger BasicAuth using nginx
    
    * removed duplicate JAMBONES_REDIS_HOST in bc-sip-sidecar config
    
    * add -y switch to apache2-utils
    
    * added apache2-utils to jambonz-mini packer nginx script
    
    * added jaeger basic auth parameters to jambonz-mini cloudformation

--- packer/jambonz-mini/scripts/install_nginx.sh
+++ packer/jambonz-mini/scripts/install_nginx.sh
@@ -4,6 +4,9 @@ echo "installing nginx"
 
 sudo apt-get install -y nginx
 
+echo "installing apache utils for htpasswd"
+sudo apt-get install -y apache2-utils
+
 cd /etc/nginx/sites-available
 sudo mv /tmp/nginx.default default
 
--- packer/jambonz-web-server/scripts/install_nginx.sh
+++ packer/jambonz-web-server/scripts/install_nginx.sh
@@ -4,6 +4,9 @@ echo "installing nginx"
 
 sudo apt-get install -y nginx
 
+echo "installing apache utils for htpasswd"
+sudo apt-get install -y apache2-utils
+
 sudo systemctl enable nginx
 sudo systemctl restart nginx
 
