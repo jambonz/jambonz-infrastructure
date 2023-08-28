b47be01853601512018c8435ce9db5d4a9fae778
Dave Horton <daveh@beachdognet.com>
2023-08-28 08:03:58 -0400

    change to use premade jambonz debian-11 image as base ami

--- packer/jambonz-sbc-sip/aws/scripts/install_os_tuning.sh
+++ packer/jambonz-sbc-sip/aws/scripts/install_os_tuning.sh
@@ -14,3 +14,16 @@ vm.dirty_writeback_centisecs=100
 EOT'
 
 sudo cp /tmp/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
+
+# disable ipv6
+echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf > /dev/null
+echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf > /dev/null
+sudo sysctl -p
+
+# install latest cmake
+cd /usr/local/src
+wget https://github.com/Kitware/CMake/archive/refs/tags/v3.27.4.tar.gz
+tar xvfz v3.27.4.tar.gz
+cd CMake-3.27.4
+./bootstrap && make -j 4 && sudo make install
+cmake --version
