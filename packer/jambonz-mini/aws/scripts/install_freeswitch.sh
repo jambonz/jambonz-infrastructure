c3e7ae5c78cafb3e2f14d095069bfe40e6b0a00b
Dave Horton <daveh@beachdognet.com>
2023-09-18 16:18:44 -0400

    more updates to freeswitch 1.10.10 and other changes

--- packer/jambonz-mini/aws/scripts/install_freeswitch.sh
+++ packer/jambonz-mini/aws/scripts/install_freeswitch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-FREESWITCH_VERSION=v1.10.10
-SPAN_DSP_VERSION=0d2e6ac
-GRPC_VERSION=v1.57.0
-GOOGLE_API_VERSION=29374574304f3356e64423acc9ad059fe43f09b5
-#AWS_SDK_VERSION=1.11.143 # newer but buggy with s2n_init crashes and weird slowdown on voice playout in FS
+FREESWITCH_VERSION=v1.10.5
+GRPC_VERSION=c66d2cc
+#GRPC_VERSION=v1.39.1
+#GOOGLE_API_VERSION=v1p1beta1-speech
+GOOGLE_API_VERSION=e9da6f8b469c52b83f900e820be30762e9e05c57
 AWS_SDK_VERSION=1.8.129
-LWS_VERSION=v4.3.2
-MODULES_VERSION=v0.8.0
+LWS_VERSION=v3.2.3
+MODULES_VERSION=v0.7.2
 
 echo "freeswitch version to install is ${FREESWITCH_VERSION}"
 echo "drachtio modules version to install is ${MODULES_VERSION}"
@@ -51,7 +51,8 @@ cd freeswitch/libs
 git clone https://github.com/drachtio/nuance-asr-grpc-api.git -b main
 git clone https://github.com/drachtio/riva-asr-grpc-api.git -b main
 git clone https://github.com/drachtio/soniox-asr-grpc-api.git -b main
-git clone https://github.com/freeswitch/spandsp.git && cd spandsp && git checkout ${SPAN_DSP_VERSION} && cd ..
+git clone https://github.com/freeswitch/spandsp.git -b master
+cd spandsp && git checkout 728b60abdd1a71e254b8e831e9156521d788b2b9 && cd ..
 git clone https://github.com/freeswitch/sofia-sip.git -b master
 git clone https://github.com/dpirch/libfvad.git
 git clone https://github.com/aws/aws-sdk-cpp.git -b ${AWS_SDK_VERSION}
@@ -73,6 +74,9 @@ sudo cp -r /usr/local/src/drachtio-freeswitch-modules/modules/mod_jambonz_transc
 sudo cp -r /usr/local/src/drachtio-freeswitch-modules/modules/mod_google_tts /usr/local/src/freeswitch/src/mod/applications/mod_google_tts
 sudo cp -r /usr/local/src/drachtio-freeswitch-modules/modules/mod_dialogflow /usr/local/src/freeswitch/src/mod/applications/mod_dialogflow
  
+sudo sed -i -r -e 's/(.*AM_CFLAGS\))/\1 -g -O0/g' /usr/local/src/freeswitch/src/mod/applications/mod_audio_fork/Makefile.am
+sudo sed -i -r -e 's/(.*-std=c++11)/\1 -g -O0/g' /usr/local/src/freeswitch/src/mod/applications/mod_audio_fork/Makefile.am
+
 # copy Makefiles and patches into place
 cp /tmp/configure.ac.extra /usr/local/src/freeswitch/configure.ac
 cp /tmp/Makefile.am.extra /usr/local/src/freeswitch/Makefile.am
@@ -84,67 +88,44 @@ cp /tmp/switch_core_media.c.patch /usr/local/src/freeswitch/src
 cp /tmp/mod_avmd.c.patch /usr/local/src/freeswitch/src/mod/applications/mod_avmd
 cp /tmp/mod_httapi.c.patch /usr/local/src/freeswitch/src/mod/applications/mod_httapi
 
+# patch freeswitch
 cd /usr/local/src/freeswitch/src
-echo patching switch_rtp
 patch < switch_rtp.c.patch
-echo patching switch_core_media
 patch < switch_core_media.c.patch
 cd /usr/local/src/freeswitch/src/mod/applications/mod_avmd
-echo patching mod_avmd
 patch < mod_avmd.c.patch
 cd /usr/local/src/freeswitch/src/mod/applications/mod_httapi
-echo patching mod_httapi
 patch < mod_httapi.c.patch
 
 # build libwebsockets
-echo building lws
 cd /usr/local/src/libwebsockets
 sudo mkdir -p build && cd build && sudo cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo && sudo make && sudo make install
 
 # build libfvad
 cd /usr/local/src/freeswitch/libs/libfvad
-# use our version of libfvad configure.ac - should only do this on debian 12
-if [ "$DISTRO" == "debian-12" ]; then
-  echo "patching libfvad configure.ac to remove deprecated commands"
-  sudo cp /tmp/configure.ac.libfvad configure.ac
-fi
-echo building libfvad
 sudo autoreconf -i && sudo ./configure && sudo make -j 4 && sudo make install
 
 # build spandsp
-echo building spandsp
 cd /usr/local/src/freeswitch/libs/spandsp
 ./bootstrap.sh && ./configure && make -j 4 && sudo make install
 
 # build sofia
-echo building sofia
 cd /usr/local/src/freeswitch/libs/sofia-sip
 ./bootstrap.sh && ./configure && make -j 4 && sudo make install
 
 # build aws-c-common
-echo building aws-c-common
 cd /usr/local/src/freeswitch/libs/aws-c-common
 mkdir -p build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS="-Wno-unused-parameter"
 make -j 4 && sudo make install
 
 # build aws-sdk-cpp
-echo building aws-sdk-cpp
 cd /usr/local/src/freeswitch/libs/aws-sdk-cpp
-git submodule update --init --recursive
 mkdir -p build && cd build
 cmake .. -DBUILD_ONLY="lexv2-runtime;transcribestreaming" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS="-Wno-unused-parameter"
-if [ "$DISTRO" == "debian-12" ]; then
-  echo "patching aws-sdk-cpp to fix debian 12 build"
-  sudo sed -i 's/uint8_t arr\[16\];/uint8_t arr\[16\] = {0};/g' /usr/local/src/freeswitch/libs/aws-sdk-cpp/build/.deps/build/src/AwsCCommon/tests/byte_buf_test.c
-  sudo sed -i 's/char filename_array\[64\];/char filename_array\[64\] = {0};/g' /usr/local/src/freeswitch/libs/aws-sdk-cpp/build/.deps/build/src/AwsCCommon/tests/logging/logging_test_utilities.c
-  cmake .. -DBUILD_ONLY="lexv2-runtime;transcribestreaming" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS="-Wno-unused-parameter"
-fi
-sudo make -j 4 && sudo make install
-sudo find /usr/local/src/freeswitch/libs/aws-sdk-cpp/ -type f -name "*.pc" | sudo xargs cp -t /usr/local/lib/pkgconfig/
+make -j 4 && sudo make install
 
 # build grpc
-echo building grpc
 cd /usr/local/src/grpc
 git submodule update --init --recursive
 mkdir -p cmake/build
@@ -154,7 +135,6 @@ make -j 4
 sudo make install
 
 # build googleapis
-echo building googleapis
 cd /usr/local/src/freeswitch/libs/googleapis
 echo "Ref: https://github.com/GoogleCloudPlatform/cpp-samples/issues/113"
 sed -i 's/\$fields/fields/' google/maps/routes/v1/route_service.proto
@@ -180,7 +160,7 @@ LANGUAGE=cpp make
 echo "building freeswitch"
 cd /usr/local/src/freeswitch
 sudo ./bootstrap.sh -j
-sudo ./configure --enable-tcmalloc=yes --with-lws=yes --with-extra=yes --with-aws=yes
+sudo ./configure --enable-tcmalloc=yes --with-lws=yes --with-extra=yes
 sudo make -j 4
 sudo make install
 sudo make cd-sounds-install cd-moh-install
--- packer/jambonz-mini/aws/scripts/install_os_tuning.sh
+++ packer/jambonz-mini/aws/scripts/install_os_tuning.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DISTRO=$1
+
 sudo sed -i '/# End of file/i *                hard       nofile          65535'  /etc/security/limits.conf
 sudo sed -i '/# End of file/i *                soft       nofile          65535'  /etc/security/limits.conf
 sudo sed -i '/# End of file/i root             hard       nofile          65535'  /etc/security/limits.conf
@@ -20,9 +22,11 @@ echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf > /dev/
 echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf > /dev/null
 
 # install latest cmake
-cd /usr/local/src
-wget https://github.com/Kitware/CMake/archive/refs/tags/v3.27.4.tar.gz
-tar xvfz v3.27.4.tar.gz
-cd CMake-3.27.4
-./bootstrap && make -j 4 && sudo make install
-cmake --version
+if [ "$DISTRO" == "debian-12" ]; then
+  cd /usr/local/src
+  wget https://github.com/Kitware/CMake/archive/refs/tags/v3.27.4.tar.gz
+  tar xvfz v3.27.4.tar.gz
+  cd CMake-3.27.4
+  ./bootstrap && make -j 4 && sudo make install
+  cmake --version
+fi
